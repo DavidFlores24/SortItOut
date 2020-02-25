52958606a187a7fc2537517300f504df85068caa
David Flores <david.fgardens@gmail.com>
2020-02-25 23:16:20 +0000

    feat: connect db and add comments

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+// dependencies
 const express = require("express");
-// const connectDB = require("./config/db");
+const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 
 dotenv.config();
+
+// routes
 const cloudUtils = require("./src/routes/api/cloudUtils");
-// connectDB();
 const recycleRequest = require("./src/routes/api/recycleRequest");
+
+// connect the DB
+connectDB();
+
+// configure server
 app = express();
 app.use(
   bodyParser.urlencoded({
@@ -15,12 +22,14 @@ app.use(
 );
 app.use(bodyParser.json());
 
+// define routes
 app.use("/services", cloudUtils);
-app.get("/", (req, res) => res.send("hello world"));
 app.use("/recycle-requests", recycleRequest);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+// default get
+app.get("/", (req, res) => res.send("API for the Sort it Out chatbot"));
+
+// start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
