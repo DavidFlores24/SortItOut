14f4c3b8a9d31b4d43583b1e5c1b5acf1369153a
David Flores <david.fgardens@gmail.com>
2020-02-26 01:45:08 +0000

    feat: return material on response

--- src/routes/api/recycleRequest.js
+++ src/routes/api/recycleRequest.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const RecycleRequest = require("../../models/RecycleRequest");
+const getImageInfo = require("../../APIs/azure/getImageInfo");
 
 router.post("/", (req, res) => {
   RecycleRequest.create(req.body)
-    .then(request => {
-      res.status(200).send(request);
+    .then(async request => {
+      const material = await getImageInfo(request.image_url);
+      res.status(200).send({ request, material });
     })
     .catch(err => {
       res.status(400).send(err.message);
