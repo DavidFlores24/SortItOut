2dfe4070671ba2fd0e59a48a8a484ee83364c22c
David Flores <david.fgardens@gmail.com>
2020-02-26 16:45:13 +0000

    reafactor process request

--- src/utils/processRequest.js
+++ src/utils/processRequest.js
@@ -1,12 +1,27 @@
 const getImageInfo = require("../APIs/azure/getImageInfo");
 const getCouncilData = require("../APIs/geoLocation/getCouncilData");
+const getRecylingData = require("./getRecyclingData");
+const RecycleResponse = require("../models/RecycleResponse");
 
 const processRequest = async request => {
   try {
-    const response = await Promise.all([
+    const info = await Promise.all([
       getImageInfo(request.image_url),
       getCouncilData(request)
     ]);
+
+    const recyclingData = getRecylingData(info[0], info[1]);
+
+    const response = {
+      request_id: request._id,
+      is_recyclable: recyclingData.isRecyclable,
+      container_material: recyclingData.material,
+      container_type: recyclingData.type,
+      council: recyclingData.council
+    };
+
+    RecycleResponse.create(response);
+
     return response;
   } catch (err) {
     console.log(err);
